a76ec6924a57f8817587b7c716a2d372f10f4d52
Blake Sergesketter <blakesergesketter@gmail.com>
2024-02-19 15:04:05 -0600

    Addition of conditional logic to require a query of a return limit. Moving on to testing.

--- app/controllers/api/v0/justinisms_controller.rb
+++ app/controllers/api/v0/justinisms_controller.rb
@@ -1,7 +1,11 @@
 class Api::V0::JustinismsController < ApplicationController
   def index 
-    limit = params[:limit] || 1 
-    justinisms = Justinism.order("RANDOM()").limit(limit)
-    render json: JustinismSerializer.new(justinisms)
+    if params[:limit].present? 
+      limit = params[:limit] 
+      justinisms = Justinism.order("RANDOM()").limit(limit)
+      render json: JustinismSerializer.new(justinisms), status: 200
+    else 
+      render json: { "errors": [{ "detail": "Couldn't find saying without parameters"}] }, status: 400
+    end 
   end
 end
