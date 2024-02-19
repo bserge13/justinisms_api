a5996af25444ee9422f165d1b31afb838cdbf74f
Blake Sergesketter <blakesergesketter@gmail.com>
2024-02-19 15:22:38 -0600

    TEST- sad path testing of api call without query params. All testing passes.

--- spec/requests/get_index_spec.rb
+++ spec/requests/get_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Justinisms Index' do 
-  describe 'GET request' do 
+  describe 'GET request- happy path' do 
     it 'returns a GET request with query-specific number of sayings' do 
       saying1 = Justinism.create!(saying: "Jack shit!")
       saying2 = Justinism.create!(saying: "What up, Snickers")
@@ -25,6 +25,29 @@ RSpec.describe 'Justinisms Index' do
       expect(ism[:attributes]).to be_a Hash 
       expect(ism[:attributes]).to have_key :saying
       expect(ism[:attributes][:saying]).to be_a String
+    end
+  end
+
+  describe 'GET request- sad path'do 
+    it 'does not return an object without the query param' do 
+      saying1 = Justinism.create!(saying: "Jack shit!")
+      saying2 = Justinism.create!(saying: "What up, Snickers")
+
+      header = { CONTENT_TYPE: 'application/json',
+      ACCEPT: 'application/json' }
+      get '/api/v0/justinisms', headers: header 
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+      expect(error).to be_a Hash 
+      expect(error).to have_key :errors 
+      expect(error[:errors]).to be_an Array 
+      expect(error[:errors][0]).to be_a Hash
+      expect(error[:errors][0]).to have_key :detail
+      expect(error[:errors][0][:detail]).to be_a String 
+      expect(error[:errors][0][:detail]).to eq("Couldn't find saying without parameters")
     end
   end
 end
